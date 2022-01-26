001182add0fd6c3cbb7452673d282465a0e63451
IT-gMA <tiendatoan@gmail.com>
2022-01-27 00:15:47 +0800

    this file splits the VOC .xml and .jpeg into 2 separate directories

--- voc_txt.py
+++ voc_txt.py
@@ -1,59 +1,36 @@
 import os
 import random
-import sys
-from pathlib import Path
+random.seed(0)
 
-if len(sys.argv) < 2:
-    print("no directory specified, please input target directory")
-    exit()
+# You gotta create thses directories b4 hand
+xmlfilepath = r'datasets/VOCdevkit/VOC2007/Annotations'
+saveBasePath = r"datasets/VOCdevkit/VOC2007/ImageSets/Main"
 
-root_path = sys.argv[1]
+trainval_percent = 0.8
+train_percent = 1
 
-xmlfilepath = root_path + 'VOC2007/Annotations/'
-os.mkdir(xmlfilepath)
-imagefilepath = root_path + 'VOC2007/JPEGImages/'
-os.mkdir(imagefilepath)
+temp_xml = os.listdir(xmlfilepath)
+total_xml = []
+for xml in temp_xml:
+    if xml.endswith(".xml"):
+        total_xml.append(xml)
 
-# Move annotations to annotations folder
-for filename in os.listdir(root_path):
-    if filename.endswith('.xml'):
-        with open(os.path.join(root_path, filename)) as f:
-            Path(root_path + filename).rename(xmlfilepath + filename)
-
-    if filename.endswith('.jpg'):
-        with open(os.path.join(root_path, filename)) as f:
-            Path(root_path + filename).rename(imagefilepath + filename)
-
-
-txtsavepath = root_path + '/VOC2007/ImageSets/Main'
-
-if not os.path.exists(root_path):
-    print("cannot find such directory: " + root_path)
-    exit()
-
-if not os.path.exists(txtsavepath):
-    os.makedirs(txtsavepath)
-
-trainval_percent = 0.9
-train_percent = 0.8
-total_xml = os.listdir(xmlfilepath)
 num = len(total_xml)
 list = range(num)
-tv = int(num * trainval_percent)
-tr = int(tv * train_percent)
+tv = int(num*trainval_percent)
+tr = int(tv*train_percent)
 trainval = random.sample(list, tv)
 train = random.sample(trainval, tr)
 
-print("train and val size:", tv)
-print("train size:", tr)
-
-ftrainval = open(txtsavepath + '/trainval.txt', 'w')
-ftest = open(txtsavepath + '/test.txt', 'w')
-ftrain = open(txtsavepath + '/train.txt', 'w')
-fval = open(txtsavepath + '/val.txt', 'w')
+print("train and val size", tv)
+print("train size", tr)
+ftrainval = open(os.path.join(saveBasePath, 'trainval.txt'), 'w')
+ftest = open(os.path.join(saveBasePath, 'test.txt'), 'w')
+ftrain = open(os.path.join(saveBasePath, 'train.txt'), 'w')
+fval = open(os.path.join(saveBasePath, 'val.txt'), 'w')
 
 for i in list:
-    name = total_xml[i][:-4] + '\n'
+    name = total_xml[i][:-4]+'\n'
     if i in trainval:
         ftrainval.write(name)
         if i in train:
@@ -66,4 +43,4 @@ for i in list:
 ftrainval.close()
 ftrain.close()
 fval.close()
-ftest.close()
+ftest .close()

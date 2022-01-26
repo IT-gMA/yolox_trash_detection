dd3a2c5949ff8b09b3cbcc02c961166ae0faa355
IT-gMA <tiendatoan@gmail.com>
2022-01-26 19:59:52 +0800

    configuration files for 2 different sizes

--- exps/example/yolox_voc/yolox_voc_l.py
+++ exps/example/yolox_voc/yolox_voc_l.py
@@ -12,8 +12,8 @@ class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
         self.num_classes = 18
-        self.depth = 0.33
-        self.width = 0.50
+        self.depth = 1.0
+        self.width = 1.0
         self.warmup_epochs = 1
 
         # ---------- transform config ------------ #

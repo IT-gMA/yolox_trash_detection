486c1815a11c9876a4f8e2bdbdf49bc21146b965
IT-gMA <tiendatoan@gmail.com>
2022-01-27 00:36:28 +0800

    modify for my custom garbage classes

--- yolox/data/datasets/voc_classes.py
+++ yolox/data/datasets/voc_classes.py
@@ -4,7 +4,7 @@
 
 # VOC_CLASSES = ( '__background__', # always index 0
 VOC_CLASSES = (
-    "aluminum wrap",
+    "aluminium wrap",
     "cardboard",
     "cigarette",
     "foil",
@@ -14,7 +14,7 @@ VOC_CLASSES = (
     "negative",
     "nylon bag",
     "paper",
-    "plastic"
+    "plastic",
     "plastic bottle",
     "plastic cup",
     "plastic film",
